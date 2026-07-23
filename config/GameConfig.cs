14d4b34374297d184853e3de0c152c6a02dd167b
galag <gal.granot95@gmail.com>
2026-07-23 08:35:06 +0300

    game config resource

---
+++ config/GameConfig.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+[GlobalClass]
+public partial class GameConfig : Resource {
+    /*=============================================================================
+    * Movement Times
+    =============================================================================*/
+    [Export] public float card_move_time = 0.2f;
+}
